6401cf55bd46ef03f78ccb66eb2702b1fa1d39f6
AngelGallegoUCM <anggal02@ucm.es>
2024-03-10 15:55:34 +0100

    Diferentes pantallas

---
+++ carrito.php
@@ -0,0 +1,129 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
+
+    <title>VirtualVenture</title>
+</head>
+
+
+<body>
+    <?php
+    require('cabecera.php')
+        ?>
+    <main>
+        
+    <div class="carritoContainer">
+        <div class="carrito">
+            <p>Carrito</p>
+            <div class="juegoCarrito">
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto" class="portada">
+                <div class="buyinformacion">
+                   <p>Nombre</p>
+                 
+                    <p class="precio">10<span>,57</span>€</p>
+                    
+                </div>
+            </div>
+
+            <div class="juegoCarrito">
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto" class="portada">
+                <div class="buyinformacion">
+                   <p>Nombre</p>
+                 
+                    <p class="precio">10<span>,57</span>€</p>
+                    
+                </div>
+            </div>
+            
+        </div>    
+        <div class="resumen">
+            <p>Resumen</p>
+            <div class="buyinformacion">
+                <p>n </p>
+                <p>productos</p>
+                <p class="precio">10<span>,57</span>€</p>
+            </div>
+            <div style="display: flex;">
+                <input type="checkbox">
+                <p>Usar puntos disponibles:</p>
+                <p class="precio">10<span>,57</span>€</p>
+            </div>
+            <div style="margin-top: 40px;">
+                <div class="buyinformacion">
+                    <p>Total </p>
+                    
+                    <p class="precio">10<span>,57</span>€</p>
+                </div>
+            </div>
+            <button>Comprar</button>
+        </div>
+    
+    </div>
+      
+
+
+    <div class="gameBuyContainer" style="margin-top: 30px;">
+        <p>Juegos similares</p>
+        <div class="gameContainer">
+            <div>
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto">
+                <div class=informacion>
+                    <p>Producto 1</p>
+                    <p class="precio">10<span>,57</span>€</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+            <div>
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto">
+                <div class=informacion>
+                    <p>Producto 1</p>
+                    <p class="precio">10<span>,57</span>€</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+            <div>
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto">
+                <div class=informacion>
+                    <p>Producto 1</p>
+                    <p class="precio">10<span>,57</span>€</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+            <div>
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto">
+                <div class=informacion>
+                    <p>Producto 1</p>
+                    <p class="precio">10<span>,57</span>€</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+            <div>
+                <img src="img/logoFoto.jpg" alt="Logotipo de la empresa como foto">
+                <div class=informacion>
+                    <p>Producto 1</p>
+                    <p class="precio">10<span>,57</span>€</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+       
+        
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <p>Prueba</p>
+    </main>
+</body>
+
+
+</html>
